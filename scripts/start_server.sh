0a8c82de27e961f67c099c6f04ec4874a71fd89a
geetanjali-web <geetanjali.puri49@gmail.com>
2025-09-29 21:31:57 +0530

    Delete scripts/start_server.sh

--- scripts/start_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/tourism_app
-echo "Starting Flask tourism app..."
-# Kill old process if running
-pkill -f "python3 app.py"
-# Start in background
-nohup python3 app.py > app.log 2>&1 &
-
